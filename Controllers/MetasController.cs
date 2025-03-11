f9babc5ddd5ce4c8ff23dc63ecf335c8bbb96972
caste <caste@JESUS>
2025-03-10 19:35:09 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/MetasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RunGymFront.controllers
+{
+    public class MetasController : Controller
+    {
+        public ActionResult Metas()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Web;
+
+namespace RunGymFront.Models
+{
+    public class Login
+    {
+        [DisplayName("Correo")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [DisplayName("Contrasena")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; }
+    }
+}
